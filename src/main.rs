9a5feee4edbedd52e73e63e90827694a9c9337de
Dan Costinas <danscostinas@gmail.com>
2025-06-03 22:35:28 +0300

    Change to simpler example

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,9 @@ pub mod activations;
 pub mod linear;
 
 struct SimpleMLP {
-    linear0: Linear<2, 100>,
-    act0: RELU<100>,
-    linear1: Linear<100, 1>,
+    linear0: Linear<2, 5000>,
+    act0: RELU<5000>,
+    linear1: Linear<5000, 1>,
 }
 
 impl SimpleMLP {
@@ -92,7 +92,7 @@ impl MSELoss {
 fn aproximate<const B: usize>(input: &SMatrix<f32, B, 2>) -> SMatrix<f32, B, 1> {
     let mut y = SMatrix::<f32, B, 1>::from_element(0.0f32);
     for (i, row) in input.row_iter().enumerate() {
-        y[(i, 0)] = 3.0f32 * f32::sin(row[(i, 0)]) + 2.0f32 * f32::cos(row[(i, 1)]);
+        y[(i, 0)] = 3.0f32 * row[(i, 0)] + 2.0f32 * row[(i, 1)];
     }
     y
 }
@@ -101,11 +101,11 @@ fn main() {
     let mut model = SimpleMLP::new();
     println!("TEST");
     let mut rng = rand::rng();
-    let steps = 10_000usize;
+    let steps = 2_000usize;
     let loss = MSELoss::new();
     let mut losses = Vec::new();
 
-    for _ in 0..steps {
+    for i in 0..steps {
         let x = rng.random::<f32>();
         let y = rng.random::<f32>();
         let input = SMatrix::<f32, 1, 2>::from_row_slice(&[x, y]);
@@ -114,9 +114,9 @@ fn main() {
         let l = loss.forward(&y, &target);
         let l_back = loss.backward(&y, &target);
         model.backwards(l_back).expect("Should not fail");
-        model.optimize(0.001f32).expect("Should not fail");
+        model.optimize(0.01f32).expect("Should not fail");
         losses.push(l);
-        println!("y: {:?}, target: {:?}, loss: {:?}", y, target, l);
+        println!("step:{i} y: {:?}, target: {:?}, loss: {:?}", y, target, l);
     }
 
     let mut file = File::create("losses.txt").expect("Should not fail");
@@ -124,8 +124,13 @@ fn main() {
         writeln!(file, "{}", num).expect("Should not fail");
     }
 
-    let input = SMatrix::<f32, 1, 2>::from_row_slice(&[70.0f32, 25.0f32]);
+    let input = SMatrix::<f32, 1, 2>::from_row_slice(&[0.10f32, 0.25f32]);
     let y = model.forward(&input);
     let control = aproximate(&input);
-    println!("y: {:?} control: {:?}", y, control);
+    println!(
+        "y: {:?} control: {:?} MSE:{:?}",
+        y,
+        control,
+        loss.forward(&y, &control)
+    );
 }
